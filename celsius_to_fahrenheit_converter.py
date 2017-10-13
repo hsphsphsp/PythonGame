337c9580bb2281c76d90f82f2cc2e13ce935fd8e
Alegruz <alegruz@khu.ac.kr>
2017-10-13 16:53:50 +0900

    Create celsius_to_fahrenheit_converter.py

---
+++ celsius_to_fahrenheit_converter.py
@@ -0,0 +1,6 @@
+def celsius_to_fahrenheit(degree):
+    return (9/5) * degree + 32
+
+degree = float(input("Input Celsius data : "))
+
+print("Result of the Celsius to Fahrenheit Conversion is :", celsius_to_fahrenheit(30))
